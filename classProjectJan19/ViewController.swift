cad24c074e19704a9847e2fe42a385c9ecd8197a
Quinn <qwienke13@gmail.com>
2023-01-19 08:10:16 -0700

    Initial Commit

---
+++ classProjectJan19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  classProjectJan19
+//
+//  Created by Quinn Wienke on 1/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
